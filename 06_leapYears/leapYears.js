3a281a978de16e097f7f235c8b5cc47ec0f45023
AndreiAlex831 <131880722+AndreiAlex831@users.noreply.github.com>
2024-07-24 11:28:57 +0300

    done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
-};
-
+const leapYears = function(year) {
+    const divByFour = year % 4 === 0;
+    const divByOneH = year % 100 === 0;
+    const divByFourH = year % 400 === 0;
+    if (divByFour && (!divByOneH || divByFourH)) {
+        return true
+    }else{
+        return false
+    }
+}
+leapYears(1985)
 // Do not edit below this line
 module.exports = leapYears;
