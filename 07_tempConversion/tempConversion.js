c312e5e3a7df0e935f74939354152d0ce0182004
AndreiAlex831 <131880722+AndreiAlex831@users.noreply.github.com>
2024-07-27 11:48:05 +0300

    done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit, celsius, roundedCelsius) {
+  celsius= (fahrenheit - 32) * 5 / 9;
+  roundedCelsius = parseFloat(celsius.toFixed(1));
+  return roundedCelsius;
 };
+convertToCelsius(32 )// Fahrenheit to Celsius, should return 0
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius, fahrenheit, roundedFahrenheit) {
+  fahrenheit = (celsius * 9 / 5) + 32;
+  roundedFahrenheit = parseFloat(fahrenheit.toFixed(1));
+  return roundedFahrenheit;
 };
-
+convertToFahrenheit(0) // Celsius to Fahrenheit, should return 32
+// Formula
+// (1°F − 32) × 5/9 = -17.
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
